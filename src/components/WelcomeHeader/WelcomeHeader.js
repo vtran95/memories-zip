c02106bec94b961c4cc3da29edfe4bb196f38ee7
Vincent Tran <vintran1995@gmail.com>
2019-02-07 23:11:05 -0800

    updated cover photo

--- src/components/WelcomeHeader/WelcomeHeader.js
+++ src/components/WelcomeHeader/WelcomeHeader.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './WelcomeHeader.css';
+import bg from '../../images/cover.jpg';
 
 const WelcomeHeader = () => {
     return (
-        <header style={{backgroundImage: "url('http://www.ayk-i.com/wp-content/uploads/2015/10/KYRFNLK23V.jpg')"}} className="masthead">
+        <header style={{backgroundImage: "url("+bg+")"}} className="masthead">
             <div className="row WelcomeHeader">
-                <div className="WelcomeHeader-div col offset-s3">
+                <div className="WelcomeHeader-div col">
                     <h1 className="Welcome-title">Live for moments</h1>
-                    <h1 className="Welcome-title">you can't put into words</h1>
+                    <h1 className="Welcome-title">you can't put into</h1>
+                    <h1 className="Welcome-title-words">WORDS</h1>
                 </div>
             </div>
         </header>
