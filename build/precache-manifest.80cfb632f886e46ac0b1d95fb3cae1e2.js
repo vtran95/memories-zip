9bc96230638503498da64c8cedca072ee07472ab
Vincent Tran <vintran1995@gmail.com>
2020-03-06 01:41:39 -0800

    fixed node version

---
+++ build/precache-manifest.80cfb632f886e46ac0b1d95fb3cae1e2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a676f794e082c8d5820e30a6d690be70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04ebf575986a7c827d71",
+    "url": "/static/css/main.87c0a0c5.chunk.css"
+  },
+  {
+    "revision": "563b5de4f1eba47a314a",
+    "url": "/static/js/2.b959dd14.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.b959dd14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04ebf575986a7c827d71",
+    "url": "/static/js/main.90d4a819.chunk.js"
+  },
+  {
+    "revision": "242a460ffec4d4f1ee92",
+    "url": "/static/js/runtime-main.d636c920.js"
+  },
+  {
+    "revision": "18f9ec5f0daaad06a23c24c9381e14ff",
+    "url": "/static/media/cover.18f9ec5f.jpg"
+  },
+  {
+    "revision": "223b7964e70d52f38c182f5150eddb9a",
+    "url": "/static/media/step1.223b7964.jpg"
+  },
+  {
+    "revision": "17dc842941062662920bb59c4a92033e",
+    "url": "/static/media/step2.17dc8429.jpg"
+  },
+  {
+    "revision": "108a1a0fdb1161a96958118b400a18a4",
+    "url": "/static/media/step3.108a1a0f.jpg"
+  }
+]);
